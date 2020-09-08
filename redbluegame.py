fed2b63e9414f3f7cfb14eae1137f823bf4f2640
Shuyi Fang <66406517+Terence0101@users.noreply.github.com>
2020-09-08 16:27:53 +0800

    Create redbluegame.py

---
+++ redbluegame.py
@@ -0,0 +1,18 @@
+score = 0
+combo = 0
+combomax = 0
+
+computer = input()
+play = input()
+
+for i in range (len(computer)):
+    if computer[i] == '-':
+        continue
+    elif computer[i] == play[i]:
+        combo += 1
+        score += combo*100
+        if combo > combomax:
+            combomax = combo
+    else:
+        combo = 0
+print(score,combomax)
